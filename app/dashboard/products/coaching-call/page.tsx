4d6cde063bf9a48006e7f6dc9725c3e6c95dc9fa
User <HenokBza>
2026-06-29 00:28:51 -0400

    latest creatorstore update

---
+++ app/dashboard/products/coaching-call/page.tsx
@@ -0,0 +1,7 @@
+export default function CoachingCallPage() {
+  return (
+    <div>
+      <h1>Coaching Call</h1>
+    </div>
+  );
+}
---
+++ app/dashboard/products/lead-magnet/page.tsx
@@ -0,0 +1,7 @@
+export default function leadmagnetPage() {
+  return (
+    <div>
+      <h1>lead magnet</h1>
+    </div>
+  );
+}
